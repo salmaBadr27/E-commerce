1c4c4e7f556a036191106fd75df80a60fb0fd7ae
salma badr <beauty_saloma@outlook.com>
2018-10-21 09:22:27 +0200

    database file

--- DB/e-commerce.sql
+++ DB/e-commerce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 17, 2018 at 01:55 PM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2018 at 09:21 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,24 +28,45 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `admin_id` int(10) UNSIGNED NOT NULL,
   `admin_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `admin_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `admin_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `admin_phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `admin_phone`, `created_at`, `updated_at`) VALUES
-(1, 'salma badr', 'beauty_saloma@outlook.com', '202cb962ac59075b964b07152d234b70', '01285507936', NULL, NULL);
+(1, 'salma', 'beauty_saloma@hotmail.com', '202cb962ac59075b964b07152d234b70', '01285507936', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(10) UNSIGNED NOT NULL,
+  `brand_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publication_status` tinyint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_description`, `publication_status`, `created_at`, `updated_at`) VALUES
+(2, 'zara', 'fffffffffyes', 1, NULL, NULL),
+(3, 'apple', 'iphone', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -53,30 +74,48 @@ INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, 
 -- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `categories` (
+  `category_id` int(10) UNSIGNED NOT NULL,
   `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category_description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `publication_status` tinyint(2) DEFAULT '0',
+  `publication_status` tinyint(4) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `publication_status`, `created_at`, `updated_at`) VALUES
-(1, 'Women', 'this is women category', 1, '2018-10-17 11:19:02', '2018-10-17 11:19:02'),
-(2, 'Men is', 'this is men category', 0, NULL, NULL),
-(9, 'Glasses', 'this is glasses category yes', 1, NULL, NULL),
-(4, 'Kids', 'fff', 1, NULL, NULL),
-(5, 'Fashion', 'fff', 1, NULL, NULL),
-(6, 'Bages', 'ddd', 1, NULL, NULL),
-(7, 'Shoses', 'ffffffffff', 1, NULL, NULL),
-(8, 'others', 'hhhhh', 1, NULL, NULL);
+(1, 'Men', 'men cat', 1, NULL, NULL),
+(2, 'Women', 'women catygory', 1, NULL, NULL),
+(3, 'Kids', 'kids cat', 0, NULL, NULL),
+(5, 'fashion', 'cccccccccc', 1, NULL, NULL),
+(6, 'Electronic', 'this is mobile category', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(10) UNSIGNED NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `customer_name`, `customer_email`, `password`, `mobile_number`, `created_at`, `updated_at`) VALUES
+(1, 'salma', 'beauty_saloma@outlook.com', '202cb962ac59075b964b07152d234b70', '01285507936', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -84,13 +123,11 @@ INSERT INTO `categories` (`category_id`, `category_name`, `category_description`
 -- Table structure for table `migrations`
 --
 
-DROP TABLE IF EXISTS `migrations`;
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -98,7 +135,322 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2018_10_16_194824_create_admin_table', 1),
-(2, '2018_10_17_103051_create_categories_table', 2);
+(2, '2018_10_17_103051_create_categories_table', 1),
+(3, '2018_10_17_172738_create_brands_table', 2),
+(4, '2018_10_17_182844_create_products_table', 3),
+(5, '2018_10_17_184022_create_product_table', 4),
+(6, '2018_10_19_131213_create_slider_table', 5),
+(7, '2018_10_19_203348_create_customer_table', 6),
+(8, '2018_10_19_205601_create_shipping_table', 7),
+(9, '2018_10_20_191626_create_payment_table', 8),
+(10, '2018_10_20_191748_create_order_table', 8),
+(11, '2018_10_20_191821_create_order_details_table', 8),
+(12, '2018_10_20_193902_create_payments_table', 9),
+(13, '2018_10_20_193958_create_orders_details_table', 9),
+(14, '2018_10_20_194036_create_orders_table', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(10) UNSIGNED NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `shipping_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `order_total` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_status` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `shipping_id`, `payment_id`, `order_total`, `order_status`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, 2, '4,000.00', 'pending', NULL, NULL),
+(2, 1, 2, 3, '4,000.00', 'pending', NULL, NULL),
+(3, 1, 3, 4, '0.00', 'pending', NULL, NULL),
+(4, 1, 4, 5, '4,000.00', 'pending', NULL, NULL),
+(5, 1, 4, 6, '0.00', 'pending', NULL, NULL),
+(6, 1, 4, 7, '0.00', 'pending', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_details`
+--
+
+CREATE TABLE `orders_details` (
+  `order_details_id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_sales_quantity` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders_details`
+--
+
+INSERT INTO `orders_details` (`order_details_id`, `order_id`, `product_id`, `product_name`, `product_price`, `product_sales_quantity`, `created_at`, `updated_at`) VALUES
+(1, 1, 13, 'iphone 8', '2000', '2', NULL, NULL),
+(2, 2, 13, 'iphone 8', '2000', '2', NULL, NULL),
+(3, 4, 15, 'mobile', '2000', '2', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int(10) UNSIGNED NOT NULL,
+  `payment_method` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payment_status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`payment_id`, `payment_method`, `payment_status`, `created_at`, `updated_at`) VALUES
+(1, 'handcash', 'pending', NULL, NULL),
+(2, 'handcash', 'pending', NULL, NULL),
+(3, 'handcash', 'pending', NULL, NULL),
+(4, 'handcash', 'pending', NULL, NULL),
+(5, 'handcash', 'pending', NULL, NULL),
+(6, 'handcash', 'pending', NULL, NULL),
+(7, 'handcash', 'pending', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `product_short_description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_long_description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_price` double(8,2) NOT NULL,
+  `product_size` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_color` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publication_status` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `category_id`, `brand_id`, `product_short_description`, `product_long_description`, `product_price`, `product_size`, `product_color`, `product_name`, `product_image`, `publication_status`, `created_at`, `updated_at`) VALUES
+(13, 2, 3, 'ffffff', 'ffffffff', 2000.00, 'large', 'black', 'mobile', 'backend/img/products/F27upQQheirAD6OtEBzP.jpg', 1, NULL, NULL),
+(14, 6, 2, 'ffffff', 'ffffffff', 2000.00, 'large', 'black', 'mobileg', 'backend/img/products/bdQd8LytoIh8gd0ZaTVC.jpg', 1, NULL, NULL),
+(15, 2, 3, 'ffffff', 'ffffffff', 2000.00, 'large', 'black', 'mobile', 'backend/img/products/HEFsFZ6Tw0SbGv66vdjf.jpg', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE `shipping` (
+  `shipping_id` int(10) UNSIGNED NOT NULL,
+  `shipping_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shipping_first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shipping_last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shipping_address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shipping_city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shipping_mobile_number` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `shipping`
+--
+
+INSERT INTO `shipping` (`shipping_id`, `shipping_email`, `shipping_first_name`, `shipping_last_name`, `shipping_address`, `shipping_city`, `shipping_mobile_number`, `created_at`, `updated_at`) VALUES
+(1, 'beauty_saloma@outlook.com', 'salma', 'badr', 'portfoaad', 'egypt', '01285507936', NULL, NULL),
+(2, 'beauty_saloma@outlook.com', 'salma', 'badr', 'portfoaad', 'egypt', '01285507936', NULL, NULL),
+(3, 'beauty_saloma@outlook.com', 'salma', 'badr', 'portfoaad', 'egypt', '01285507936', NULL, NULL),
+(4, 'beauty_saloma@outlook.com', 'salma', 'badr', 'portfoaad', 'egypt', '01285507936', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `slider_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `slider_image` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publication_status` tinyint(2) NOT NULL DEFAULT '0',
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`slider_id`, `created_at`, `slider_image`, `publication_status`, `updated_at`) VALUES
+(7, NULL, 'backend/img/sliders/Z74z1RwviTk93eJuzIUc.jpg', 1, NULL),
+(8, NULL, 'backend/img/sliders/W9vjZicmsyzR8NZp2NaE.jpg', 1, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `orders_details`
+--
+ALTER TABLE `orders_details`
+  ADD PRIMARY KEY (`order_details_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`shipping_id`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`slider_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders_details`
+--
+ALTER TABLE `orders_details`
+  MODIFY `order_details_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `shipping`
+--
+ALTER TABLE `shipping`
+  MODIFY `shipping_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `slider_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
